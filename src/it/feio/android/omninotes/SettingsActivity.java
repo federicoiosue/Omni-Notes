13ce1398428ab65a1f0c6a0f2450255d0408a311
Federico Iosue <federico.iosue@gmail.com>
2013-11-05 15:00:55 +0100

    Reset all data in settings

--- src/it/feio/android/omninotes/SettingsActivity.java
+++ src/it/feio/android/omninotes/SettingsActivity.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.util.Log;
@@ -28,39 +32,38 @@ public class SettingsActivity extends PreferenceActivity {
 		addPreferencesFromResource(R.xml.settings);
 
 		// Evento di pressione sul pulsante di reset delle impostazioni
-//		Preference resetData = findPreference("reset_all_data");
-//		resetData.setOnPreferenceClickListener(new OnPreferenceClickListener() {
-//
-//			@Override
-//			public boolean onPreferenceClick(Preference arg0) {
-//				AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
-//
-//				// set dialog message
-//				alertDialogBuilder.setMessage("Resettare?").setCancelable(false)
-//						.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-//
-//							public void onClick(DialogInterface dialog, int id) {
-//								SharedPreferences prefs = PreferenceManager
-//										.getDefaultSharedPreferences(context);
-//								prefs.edit().clear().commit();
-//								Log.i(Constants.TAG, "Impostazioni e dati resettati");
-//							}
-//						}).setNegativeButton("No", new DialogInterface.OnClickListener() {
-//
-//							public void onClick(DialogInterface dialog, int id) {
-//								dialog.cancel();
-//							}
-//						});
-//
-//				// create alert dialog
-//				AlertDialog alertDialog = alertDialogBuilder.create();
-//
-//				// show it
-//				alertDialog.show();
-//				return false;
-//			}
-//
-//		});
+		Preference resetData = findPreference("reset_all_data");
+		resetData.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+
+			@Override
+			public boolean onPreferenceClick(Preference arg0) {
+				AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+
+				// set dialog message
+				alertDialogBuilder.setMessage(getString(R.string.reset_all_data_confirmation))
+						.setCancelable(false).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+							public void onClick(DialogInterface dialog, int id) {
+								PreferenceManager.getDefaultSharedPreferences(context).edit().clear()
+										.commit();
+								Log.i(Constants.TAG, "Settings back to default");
+							}
+						}).setNegativeButton("No", new DialogInterface.OnClickListener() {
+
+							public void onClick(DialogInterface dialog, int id) {
+								dialog.cancel();
+							}
+						});
+
+				// create alert dialog
+				AlertDialog alertDialog = alertDialogBuilder.create();
+
+				// show it
+				alertDialog.show();
+				return false;
+			}
+
+		});
 
 
 		// Evento di pressione sul pulsante di About
@@ -69,7 +72,7 @@ public class SettingsActivity extends PreferenceActivity {
 
 			@Override
 			public boolean onPreferenceClick(Preference preference) {
-				Log.i(Constants.TAG, "About clicked");	
+				Log.i(Constants.TAG, "About clicked");
 				Intent intent = new Intent(Intent.ACTION_VIEW);
 				intent.setData(Uri.parse(getResources().getString(R.string.dev_googleplus)));
 				startActivity(intent);
@@ -77,5 +80,5 @@ public class SettingsActivity extends PreferenceActivity {
 			}
 		});
 	}
-	
+
 }
