d1d440776ed9ac892d9a84df6a45321757f70e3e
Federico Iosue <federico.iosue@gmail.com>
2013-11-05 16:18:34 +0100

    Added about popup

---
+++ src/it/feio/android/omninotes/AboutActivity.java
@@ -0,0 +1,88 @@
+package it.feio.android.omninotes;
+
+import it.feio.android.omninotes.utils.Constants;
+import android.net.Uri;
+import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RoundRectShape;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.NavUtils;
+
+public class AboutActivity extends FragmentActivity {
+
+	@SuppressWarnings("deprecation")
+	@SuppressLint("NewApi") @Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_about);
+		
+		// Shape creation for the layout
+		RoundRectShape rect = new RoundRectShape(new float[] { 30, 30, 30, 30, 30, 30, 30, 30 }, null, null);
+		ShapeDrawable bg = new ShapeDrawable(rect);
+		bg.getPaint().setColor(Color.parseColor(getString(R.color.about_bg)));
+		int sdk = android.os.Build.VERSION.SDK_INT;
+		if (sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+			getWindow().getDecorView().getRootView().setBackgroundDrawable(bg);
+		} else {
+			getWindow().getDecorView().getRootView().setBackground(bg);
+		}
+				
+		// Icons click management
+		ImageView googleplus = (ImageView) findViewById(R.id.googleplus);
+		googleplus.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Log.i(Constants.TAG, "Google+ clicked");
+				Intent intent = new Intent(Intent.ACTION_VIEW);
+				intent.setData(Uri.parse(getResources().getString(R.string.dev_googleplus)));
+				startActivity(intent);
+			}
+		});
+		ImageView github = (ImageView) findViewById(R.id.github);
+		github.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Log.i(Constants.TAG, "Github clicked");
+				Intent intent = new Intent(Intent.ACTION_VIEW);
+				intent.setData(Uri.parse(getResources().getString(R.string.dev_github)));
+				startActivity(intent);
+			}
+		});
+	}
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.about, menu);
+		return true;
+	}
+
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case android.R.id.home:
+				// This ID represents the Home or Up button. In the case of this
+				// activity, the Up button is shown. Use NavUtils to allow users
+				// to navigate up one level in the application structure. For
+				// more details, see the Navigation pattern on Android Design:
+				//
+				// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+				//
+				NavUtils.navigateUpFromSameTask(this);
+				return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+}
