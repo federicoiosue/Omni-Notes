f08e5f361254e71e195fcac4e972db259f7da335
Federico Iosue <federico.iosue@gmail.com>
2013-12-04 16:08:02 +0100

    Added some utility classes. Work still to be done on them.

---
+++ src/it/feio/android/omninotes/async/BitmapLoaderTask.java
@@ -0,0 +1,53 @@
+package it.feio.android.omninotes.async;
+
+import it.feio.android.omninotes.utils.BitmapDecoder;
+import it.feio.android.omninotes.utils.Constants;
+
+import java.io.FileNotFoundException;
+import java.lang.ref.WeakReference;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.ImageView;
+
+public class BitmapLoaderTask extends AsyncTask<Uri, Void, Bitmap> {
+
+	private final Context ctx;
+	private final WeakReference<ImageView> imageViewReference;
+	private int reqWidth;
+	private int reqHeight;
+
+	public BitmapLoaderTask(Context ctx, ImageView imageView) {
+		this.ctx = ctx;
+		imageViewReference = new WeakReference<ImageView>(imageView);
+	}
+
+	@Override
+	protected Bitmap doInBackground(Uri... params) {
+		Bitmap bmp = null;
+		Uri uri = params[0];
+		reqWidth = Constants.THUMBNAIL_SIZE;
+		reqHeight = Constants.THUMBNAIL_SIZE;
+
+		try {
+			bmp = BitmapDecoder.decodeSampledFromUri(ctx, uri, reqWidth,
+					reqHeight);
+		} catch (FileNotFoundException e) {
+			Log.e(Constants.TAG, "Image not found");
+		}
+		return bmp;
+	}
+
+	@Override
+	protected void onPostExecute(Bitmap bitmap) {
+		if (imageViewReference != null && bitmap != null) {
+			final ImageView imageView = imageViewReference.get();
+			if (imageView != null) {
+				imageView.setImageBitmap(bitmap);
+				imageView.invalidate();
+			}
+		}
+	}
+}
---
+++ src/it/feio/android/omninotes/db/DbExporter.java
@@ -0,0 +1,171 @@
+package it.feio.android.omninotes.db;
+
+import it.feio.android.omninotes.utils.Constants;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Environment;
+import android.util.Log;
+
+/**
+ * Android DataExporter that allows the passed in SQLiteDatabase to be exported
+ * to external storage (SD card) in an XML format.
+ * 
+ * To backup a SQLite database you need only copy the database file itself (on
+ * Android /data/data/APP_PACKAGE/databases/DB_NAME.db) -- you *don't* need this
+ * export to XML step.
+ * 
+ * XML export is useful so that the data can be more easily transformed into
+ * other formats and imported/exported with other tools (not for backup per se).
+ * 
+ * The kernel of inspiration for this came from:
+ * http://ashwinrayaprolu.wordpress.com/2011/03/15/android-database-example-database-usage-asynctask-database-export/. 
+ * (Though I have made many changes/updates here, I did initially start from that article.)
+ * 
+ * 
+ */
+public class DbExporter {
+
+	private static final String DATASUBDIRECTORY = "exampledata";
+
+	private SQLiteDatabase db;
+	private XmlBuilder xmlBuilder;
+
+	public DbExporter(SQLiteDatabase db) {
+		this.db = db;
+	}
+
+	public void export(String dbName, String exportFileNamePrefix)
+			throws IOException {
+		Log.i(Constants.TAG, "exporting database - " + dbName
+				+ " exportFileNamePrefix=" + exportFileNamePrefix);
+
+		this.xmlBuilder = new XmlBuilder();
+		this.xmlBuilder.start(dbName);
+
+		// get the tables
+		String sql = "select * from sqlite_master";
+		Cursor c = this.db.rawQuery(sql, new String[0]);
+		Log.d(Constants.TAG, "select * from sqlite_master, cur size "
+				+ c.getCount());
+		if (c.moveToFirst()) {
+			do {
+				String tableName = c.getString(c.getColumnIndex("name"));
+				Log.d(Constants.TAG, "table name " + tableName);
+
+				// skip metadata, sequence, and uidx (unique indexes)
+				if (!tableName.equals("android_metadata")
+						&& !tableName.equals("sqlite_sequence")
+						&& !tableName.startsWith("uidx")) {
+					this.exportTable(tableName);
+				}
+			} while (c.moveToNext());
+		}
+		String xmlString = this.xmlBuilder.end();
+		this.writeToFile(xmlString, exportFileNamePrefix + ".xml");
+		Log.i(Constants.TAG, "exporting database complete");
+	}
+
+	private void exportTable(final String tableName) throws IOException {
+		Log.d(Constants.TAG, "exporting table - " + tableName);
+		this.xmlBuilder.openTable(tableName);
+		String sql = "select * from " + tableName;
+		Cursor c = this.db.rawQuery(sql, new String[0]);
+		if (c.moveToFirst()) {
+			int cols = c.getColumnCount();
+			do {
+				this.xmlBuilder.openRow();
+				for (int i = 0; i < cols; i++) {
+					this.xmlBuilder.addColumn(c.getColumnName(i),
+							c.getString(i));
+				}
+				this.xmlBuilder.closeRow();
+			} while (c.moveToNext());
+		}
+		c.close();
+		this.xmlBuilder.closeTable();
+	}
+
+	private void writeToFile(String xmlString, String exportFileName)
+			throws IOException {
+		File dir = new File(Environment.getExternalStorageDirectory(),
+				DATASUBDIRECTORY);
+		if (!dir.exists()) {
+			dir.mkdirs();
+		}
+		File file = new File(dir, exportFileName);
+		file.createNewFile();
+
+		ByteBuffer buff = ByteBuffer.wrap(xmlString.getBytes());
+		FileChannel channel = new FileOutputStream(file).getChannel();
+		try {
+			channel.write(buff);
+		} finally {
+			if (channel != null)
+				channel.close();
+		}
+	}
+
+	/**
+	 * XmlBuilder is used to write XML tags (open and close, and a few
+	 * attributes) to a StringBuilder. Here we have nothing to do with IO or
+	 * SQL, just a fancy StringBuilder.
+	 * 
+	 * @author ccollins
+	 * 
+	 */
+	class XmlBuilder {
+		private static final String OPEN_XML_STANZA = "";
+		private static final String CLOSE_WITH_TICK = "'>";
+		private static final String DB_OPEN = "<database name='";
+		private static final String DB_CLOSE = "";
+		private static final String TABLE_OPEN = "<table name='";
+		private static final String TABLE_CLOSE = "";
+		private static final String ROW_OPEN = "";
+		private static final String ROW_CLOSE = "";
+		private static final String COL_OPEN = "<col name='";
+		private static final String COL_CLOSE = "";
+
+		private final StringBuilder sb;
+
+		public XmlBuilder() throws IOException {
+			this.sb = new StringBuilder();
+		}
+
+		void start(String dbName) {
+			this.sb.append(OPEN_XML_STANZA);
+			this.sb.append(DB_OPEN + dbName + CLOSE_WITH_TICK);
+		}
+
+		String end() throws IOException {
+			this.sb.append(DB_CLOSE);
+			return this.sb.toString();
+		}
+
+		void openTable(String tableName) {
+			this.sb.append(TABLE_OPEN + tableName + CLOSE_WITH_TICK);
+		}
+
+		void closeTable() {
+			this.sb.append(TABLE_CLOSE);
+		}
+
+		void openRow() {
+			this.sb.append(ROW_OPEN);
+		}
+
+		void closeRow() {
+			this.sb.append(ROW_CLOSE);
+		}
+
+		void addColumn(final String name, final String val) throws IOException {
+			this.sb.append(COL_OPEN + name + CLOSE_WITH_TICK + val + COL_CLOSE);
+		}
+	}
+}
