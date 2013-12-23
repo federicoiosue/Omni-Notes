4c80a943419c41486b7a909885e1bda569a72f56
Federico Iosue <federico.iosue@gmail.com>
2013-12-23 19:57:47 +0100

    Removed wrong sql script

--- assets/sql/upgrade-392.sql
+++
@@ -1,1 +0,0 @@
--- Dropping and re-creating attachment table due to conversion of field from TEXT to BLOBDROP table IF EXISTS attachments;CREATE	TABLE attachments	(		id INTEGER PRIMARY KEY AUTOINCREMENT,		blob blob,		note_id INTEGER	);
