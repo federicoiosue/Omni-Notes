f486d52be25ec1ed700ebc91fe5f6559fa7f6c8d
Federico Iosue <federico.iosue@gmail.com>
2014-01-07 16:06:40 +0100

    Added a colored marker to tagged notes (deactivable from settings). Modified note's layout in list and changed light text to RobotoCondensed.

--- src/it/feio/android/omninotes/models/NoteAdapter.java
+++ src/it/feio/android/omninotes/models/NoteAdapter.java
@@ -25,6 +25,7 @@ import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView.FindListener;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -48,6 +49,9 @@ public class NoteAdapter extends ArrayAdapter<Note> {
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
+		
+		Note note = values.get(position);
+		
 		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		View rowView = inflater.inflate(R.layout.note_layout, parent, false);
 
@@ -56,11 +60,11 @@ public class NoteAdapter extends ArrayAdapter<Note> {
 		TextView date = (TextView) rowView.findViewById(R.id.note_date);
 
 		// Setting note title		
-		title.setText(values.get(position).getTitle());
+		title.setText(note.getTitle());
 		
 
 		// Setting note content
-		String contentText = values.get(position).getContent();
+		String contentText = note.getContent();
 		if (contentText.length() > 0) {
 			int maxContentTextLength = 40;
 			// Long content it cutted after maxContentTextLength chars and three dots are appended as suffix
@@ -78,25 +82,34 @@ public class NoteAdapter extends ArrayAdapter<Note> {
 
 		// Evaluates the archived state...
 		ImageView archiveIcon = (ImageView)rowView.findViewById(R.id.archivedIcon);
-		int archiveIconVisibility = values.get(position).isArchived() ? View.VISIBLE : View.INVISIBLE;
+		int archiveIconVisibility = note.isArchived() ? View.VISIBLE : View.INVISIBLE;
 		archiveIcon.setVisibility(archiveIconVisibility);
 		// ... the presence of an alarm
 		ImageView alarmIcon = (ImageView)rowView.findViewById(R.id.alarmIcon);
-		int alarmIconVisibility = values.get(position).getAlarm() != null ? View.VISIBLE : View.INVISIBLE;
+		int alarmIconVisibility = note.getAlarm() != null ? View.VISIBLE : View.INVISIBLE;
 		alarmIcon.setVisibility(alarmIconVisibility);
 		// ... or attachments to show relative icon indicators	
 		ImageView attachmentIcon = (ImageView)rowView.findViewById(R.id.attachmentIcon);
-		int attachmentIconVisibility = values.get(position).getAttachmentsList().size() > 0 ? View.VISIBLE : View.INVISIBLE;
+		int attachmentIconVisibility = note.getAttachmentsList().size() > 0 ? View.VISIBLE : View.INVISIBLE;
 		attachmentIcon.setVisibility(attachmentIconVisibility);
+		
+		// Color of tag marker if note is tagged a function is active in preferences
+		if (PreferenceManager.getDefaultSharedPreferences(context).getBoolean(
+				"settings_enable_tag_marker", true)){
+			if (note.getTag() != null && note.getTag().getColor() != null) {
+				View tagMarker = rowView.findViewById(R.id.tag_marker);
+				tagMarker.setBackgroundColor(Integer.parseInt(note.getTag().getColor()));
+			}
+		}
 		
 		// Choosing if it must be shown creation date or last modification depending on sorting criteria
 		String sort_column = PreferenceManager.getDefaultSharedPreferences(context).getString(
 				Constants.PREF_SORTING_COLUMN, "");
 		if (sort_column.equals(DbHelper.KEY_CREATION))
-			date.setText(context.getString(R.string.creation) + " " + values.get(position).getCreationShort());
+			date.setText(context.getString(R.string.creation) + " " + note.getCreationShort());
 		else
 			date.setText(context.getString(R.string.last_update) + " "
-					+ values.get(position).getLastModificationShort());
+					+ note.getLastModificationShort());
 
 		// Highlighted if is part of multiselection of notes. Remember to search for child with card ui
 		View v = rowView.findViewById(R.id.card_layout);
