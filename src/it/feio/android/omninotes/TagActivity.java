fe7eeabe947fee12001a1ac5cbdad25b78d76547
Federico Iosue <federico.iosue@gmail.com>
2014-01-10 14:57:58 +0100

    Avoid TagActivity extending ActionBarActivity because of crashing on Gingerbread

--- src/it/feio/android/omninotes/TagActivity.java
+++ src/it/feio/android/omninotes/TagActivity.java
@@ -3,12 +3,16 @@ package it.feio.android.omninotes;
 import com.larswerkman.holocolorpicker.ColorPicker;
 import com.larswerkman.holocolorpicker.ColorPicker.OnColorChangedListener;
 
+import it.feio.android.omninotes.db.DbHelper;
 import it.feio.android.omninotes.models.Tag;
 import it.feio.android.omninotes.utils.Constants;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.NavUtils;
 import android.util.Log;
 import android.view.View;
@@ -18,7 +22,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class TagActivity extends BaseActivity {
+public class TagActivity extends Activity {
 
 	Tag tag;
 	EditText title;
@@ -118,14 +122,16 @@ public class TagActivity extends BaseActivity {
 		tag.setName(title.getText().toString());
 		tag.setDescription(description.getText().toString());
 		tag.setColor(String.valueOf(picker.getColor()));
+		DbHelper db = new DbHelper(this);
 		db.updateTag(tag);
-		showToast(getString(R.string.tag_saved), Toast.LENGTH_SHORT);
+		Toast.makeText(this, getString(R.string.tag_saved), Toast.LENGTH_SHORT).show();
 		goHome();
 	}
 
 	private void deleteTag() {
 		
 		// Retrieving how many notes are tagged with tag to be deleted
+		DbHelper db = new DbHelper(this);
 		int count = db.getTaggedCount(tag);
 		String msg = getString(R.string.delete_tag_confirmation).replace("$1$", String.valueOf(count));
 				
@@ -137,11 +143,13 @@ public class TagActivity extends BaseActivity {
 					@Override
 					public void onClick(DialogInterface dialog, int id) {
 						// Changes navigation if actually are shown notes associated with this tag
+						SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mActivity);
 						String navNotes = getResources().getStringArray(R.array.navigation_list_codes)[0];
 						String navigation = prefs.getString(Constants.PREF_NAVIGATION, navNotes);
 						if (String.valueOf(tag.getId()).equals(navigation))
 							prefs.edit().putString(Constants.PREF_NAVIGATION, navNotes).commit();
 						// Removes tag and edit notes associated with it
+						DbHelper db = new DbHelper(mActivity);
 						db.deleteTag(tag);
 						// Navigate back to notes list
 						goHome();
