b3219bd91d97f56a7d938382d35d974491cbde0a
Federico Iosue <federico.iosue@gmail.com>
2014-01-30 09:36:18 +0100

    Fixed crash when using Samsung or Flesky keyboards. Fixed possible crash on new tag creation.

--- src/it/feio/android/omninotes/DetailActivity.java
+++ src/it/feio/android/omninotes/DetailActivity.java
@@ -227,10 +227,10 @@ public class DetailActivity extends BaseActivity {
 		if (prefs.getBoolean("settings_enable_editor_links", false)) {
 			title.setLinksClickable(true);
 			Linkify.addLinks(title, Linkify.ALL);
-			title.setMovementMethod(LinkMovementMethod.getInstance());
+//			title.setMovementMethod(LinkMovementMethod.getInstance());
 			content.setLinksClickable(true);
 			Linkify.addLinks(content, Linkify.ALL);
-			content.setMovementMethod(LinkMovementMethod.getInstance());
+//			content.setMovementMethod(LinkMovementMethod.getInstance());
 		}
 
 		// Initialization of location TextView
