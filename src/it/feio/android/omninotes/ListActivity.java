7a3b9aed14625d75f3e2b492acce95a9a3c0fe1c
Federico Iosue <federico.iosue@gmail.com>
2014-02-09 09:17:14 +0100

    Fixed NullPointerException 	at it.feio.android.omninotes.ListActivity$ModeCallback.onDestroyActionMode(ListActivity.java:272)

--- src/it/feio/android/omninotes/ListActivity.java
+++ src/it/feio/android/omninotes/ListActivity.java
@@ -268,7 +268,7 @@ public class ListActivity extends BaseActivity {
 			while (it.hasNext()) {
 				Map.Entry mapEntry = (Map.Entry) it.next();
 				int i = (Integer) mapEntry.getKey();
-				if (mAdapter.getCount() > i && mAdapter.getItem(i) != null) {
+				if (mAdapter.getCount() > i && mAdapter.getItem(i) != null && listView.getChildAt(i) != null) {
 					mAdapter.restoreDrawable(mAdapter.getItem(i), listView.getChildAt(i).findViewById(R.id.card_layout));
 				}
 			}
