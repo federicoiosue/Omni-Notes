ac27a0f461b1df78a80182f4aaa28d4c7cf20fe9
Federico Iosue <federico.iosue@gmail.com>
2014-04-19 14:38:53 +0200

    Fixed java.lang.IllegalArgumentException: Unknown column requested: _data at android.database.DatabaseUtils.readExceptionFromParcel(DatabaseUtils.java:167) at android.database.DatabaseUtils.readExceptionFromParcel(DatabaseUtils.java:137) at android.content.ContentProviderProxy.query(ContentProviderNative.java:413) at android.content.ContentResolver.query(ContentResolver.java:461) at android.content.ContentResolver.query(ContentResolver.java:404) at it.feio.android.omninotes.utils.FileHelper.getDataColumn(FileHelper.java:124)

--- src/it/feio/android/omninotes/utils/FileHelper.java
+++ src/it/feio/android/omninotes/utils/FileHelper.java
@@ -2,11 +2,8 @@ package it.feio.android.omninotes.utils;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Calendar;
 
 import android.annotation.SuppressLint;
 import android.content.ContentUris;
@@ -17,7 +14,6 @@ import android.os.Build;
 import android.os.Environment;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
-import android.text.TextUtils;
 import android.util.Log;
 
 public class FileHelper {
@@ -127,6 +123,8 @@ public class FileHelper {
 				final int column_index = cursor.getColumnIndexOrThrow(column);
 				return cursor.getString(column_index);
 			}
+		} catch (Exception e) {
+			Log.e(Constants.TAG, "Error retrieving uri path", e);
 		} finally {
 			if (cursor != null)
 				cursor.close();
