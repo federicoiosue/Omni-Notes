095d17954e4e8d2448c28f3de61ac921183318e9
Federico Iosue <federico.iosue@gmail.com>
2014-05-10 10:39:47 +0200

    Fixed note items layout error for APIs < 17 due to RTL

--- src/it/feio/android/omninotes/models/adapters/NavDrawerCategoryAdapter.java
+++ src/it/feio/android/omninotes/models/adapters/NavDrawerCategoryAdapter.java
@@ -78,20 +78,20 @@ public class NavDrawerCategoryAdapter extends BaseAdapter {
 		// Finds elements
 		Category tag = tags.get(position);
 		
-		NoteDrawerTagAdapterViewHolder holder;
+		NoteDrawerCategoryAdapterViewHolder holder;
 	    if (convertView == null) {
 	    	convertView = inflater.inflate(layout, parent, false);
 
 			// Overrides font sizes with the one selected from user
 			Fonts.overrideTextSize(mActivity, mActivity.getSharedPreferences(Constants.PREFS_NAME, Context.MODE_MULTI_PROCESS), convertView);
 	    	
-	    	holder = new NoteDrawerTagAdapterViewHolder();
+	    	holder = new NoteDrawerCategoryAdapterViewHolder();
     		    	
 	    	holder.imgIcon = (ImageView) convertView.findViewById(R.id.icon);
 	    	holder.txtTitle = (TextView) convertView.findViewById(R.id.title);
 	    	convertView.setTag(holder);
 	    } else {
-	        holder = (NoteDrawerTagAdapterViewHolder) convertView.getTag();
+	        holder = (NoteDrawerCategoryAdapterViewHolder) convertView.getTag();
 	    }
 	
 		// Set the results into TextViews	
