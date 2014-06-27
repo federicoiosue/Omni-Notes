b7e42c1c6dc6eea8a1b1e0f2089308b55728aedd
Federico Iosue <federico.iosue@gmail.com>
2014-06-27 21:46:18 +0200

    Fixed java.lang.NullPointerException at it.feio.android.omninotes.MainActivity.getDrawerLayout(MainActivity.java:269)

--- src/it/feio/android/omninotes/MainActivity.java
+++ src/it/feio/android/omninotes/MainActivity.java
@@ -266,7 +266,7 @@ public class MainActivity extends BaseActivity implements OnDateSetListener, OnT
 
 	
 	public DrawerLayout getDrawerLayout() {
-		if (mFragmentManager.findFragmentById(R.id.navigation_drawer) != null) {
+		if (mFragmentManager!= null && mFragmentManager.findFragmentById(R.id.navigation_drawer) != null) {
 			return ((NavigationDrawerFragment)mFragmentManager.findFragmentById(R.id.navigation_drawer)).mDrawerLayout;
 		} else {
 			return null;
