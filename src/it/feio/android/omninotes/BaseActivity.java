7f602320a01ebc7becc08ff7b0e14e783c2bac36
Federico Iosue <federico.iosue@gmail.com>
2014-06-29 23:24:16 +0200

    Various fix for checklists

--- src/it/feio/android/omninotes/BaseActivity.java
+++ src/it/feio/android/omninotes/BaseActivity.java
@@ -322,7 +322,7 @@ public class BaseActivity extends ActionBarActivity {
 		}
 		
 		LayoutParams lp = new LayoutParams(300, 300);
-		lp.bottomMargin = DensityUtil.convertPixelsToDp(100, this);
+		lp.bottomMargin = DensityUtil.dpToPx(100, this);
 		lp.setMargins(12, 12, 12, getResources().getDimensionPixelSize(R.dimen.showcase_margin_bottom));
 		co.buttonLayoutParams = lp;				
 		
--- src/it/feio/android/omninotes/utils/Fonts.java
+++ src/it/feio/android/omninotes/utils/Fonts.java
@@ -31,8 +31,8 @@ public class Fonts {
 			} else if (v instanceof TextView) {
 				// ((TextView) v).setTypeface(Typeface.createFromAsset(
 				// context.getAssets(), "font.ttf"));
-				float currentSize = DensityUtil.convertPixelsToDp(
-						((TextView) v).getTextSize(), context);
+				float currentSize = DensityUtil.pxToDp(
+						Math.round(((TextView) v).getTextSize()), context);
 				int index = Arrays
 						.asList(context.getResources().getStringArray(
 								R.array.text_size_values))
