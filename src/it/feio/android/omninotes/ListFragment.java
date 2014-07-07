46b1e7afc93ca10004b6f276475f567e18b57e2f
Federico Iosue <federico.iosue@gmail.com>
2014-07-07 21:52:55 +0200

    Fixed crash when undoing archive action from outside categories

--- src/it/feio/android/omninotes/ListFragment.java
+++ src/it/feio/android/omninotes/ListFragment.java
@@ -1491,12 +1491,12 @@ public class ListFragment extends Fragment implements UndoListener, OnNotesLoade
 		// Cycles removed items to re-insert into adapter
 		for (Note note : selectedNotes) {
 			//   Manages uncategorize or archive  undo
-			if ( (undoCategorize && Navigation.checkNavigationCategory(undoCategoryList.get(note)))
-				|| undoArchive){
+			if ( (undoCategorize && !Navigation.checkNavigationCategory(undoCategoryList.get(note)))
+				|| undoArchive && Navigation.checkNavigation(Navigation.CATEGORY)){
 				if (undoCategorize) {
 					note.setCategory(undoCategoryList.get(note));
 				} else if (undoArchive) {
-					note.setArchived(!note.isArchived());
+					note.setArchived(false);
 				}
 				mAdapter.replace(note, mAdapter.getPosition(note));
 			// Manages trash undo
