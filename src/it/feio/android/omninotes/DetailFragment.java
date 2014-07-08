91772a018ec232f1a960661290ac933e358f78ae
Federico Iosue <federico.iosue@gmail.com>
2014-07-08 02:41:38 +0200

    Fixed issue not allowing exiting masked notes

--- src/it/feio/android/omninotes/DetailFragment.java
+++ src/it/feio/android/omninotes/DetailFragment.java
@@ -1010,7 +1010,7 @@ public class DetailFragment extends Fragment implements
 		}
 		
 		// Otherwise the result is passed to ListActivity
-		if (getActivity().getSupportFragmentManager() != null) {
+		if (getActivity() != null && getActivity().getSupportFragmentManager() != null) {
 			getActivity().getSupportFragmentManager().popBackStack();
 			if (getActivity().getSupportFragmentManager().getBackStackEntryCount() == 1) {
 				((MainActivity) getActivity()).getSupportActionBar().setDisplayShowTitleEnabled(true);
@@ -1665,7 +1665,7 @@ public class DetailFragment extends Fragment implements
 	void saveNote(OnNoteSaved mOnNoteSaved) {
 		
 		// Saving is avoided if note is masked and password still note inserted
-		if (note.isLocked() && !note.isPasswordChecked()) {
+		if (noteTmp.isLocked() && !noteTmp.isPasswordChecked()) {
 			return;
 		}
 		
