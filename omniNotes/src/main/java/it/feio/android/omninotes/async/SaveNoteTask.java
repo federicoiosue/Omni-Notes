d40d18ed85cd48c9b0a1e6060ab9fe05aa635e57
Federico Iosue <federico.iosue@gmail.com>
2014-11-02 15:07:09 +0100

    Fixes #62

--- omniNotes/src/main/java/it/feio/android/omninotes/async/SaveNoteTask.java
+++ omniNotes/src/main/java/it/feio/android/omninotes/async/SaveNoteTask.java
@@ -19,20 +19,21 @@ import it.feio.android.omninotes.models.listeners.OnNoteSaved;
 import it.feio.android.omninotes.receiver.AlarmReceiver;
 import it.feio.android.omninotes.utils.Constants;
 import it.feio.android.omninotes.utils.StorageManager;
+import roboguice.util.Ln;
 
 public class SaveNoteTask extends AsyncTask<Note, Void, Note> {
-	
+
 	private final Activity mActivity;
 	private boolean error = false;
 	private boolean updateLastModification = true;
 	private OnNoteSaved mOnNoteSaved;
 
-	
+
 	public SaveNoteTask(DetailFragment activity, boolean updateLastModification) {
 		this(activity, null, updateLastModification);
 	}
 
-	
+
 	public SaveNoteTask(DetailFragment activity, OnNoteSaved mOnNoteSaved, boolean updateLastModification) {
 		super();
 		mActivity = activity.getActivity();
@@ -40,41 +41,53 @@ public class SaveNoteTask extends AsyncTask<Note, Void, Note> {
 		this.updateLastModification = updateLastModification;
 	}
 
-	
+
 	@Override
 	protected Note doInBackground(Note... params) {
 		Note note = params[0];
 		purgeRemovedAttachments(note);
-		
+
 		if (!error) {
-			DbHelper db = DbHelper.getInstance(mActivity);		
+			DbHelper db = DbHelper.getInstance(mActivity);
 			// Note updating on database
 			note = db.updateNote(note, updateLastModification);
 		} else {
 			Toast.makeText(mActivity, mActivity.getString(R.string.error_saving_attachments), Toast.LENGTH_SHORT).show();
 		}
-			
+
 		return note;
 	}
-	
+
 	private void purgeRemovedAttachments(Note note) {
 		List<Attachment> deletedAttachments = note.getAttachmentsListOld();
 		for (Attachment attachment : note.getAttachmentsList()) {
 			if (attachment.getId() != 0) {
-				deletedAttachments.remove(attachment);
+                // Workaround to prevent deleting attachments if instance is changed (app restart)
+                if (deletedAttachments.indexOf(attachment) == -1) {
+                    attachment = getFixedAttachmentInstance(deletedAttachments, attachment);
+                }
+                deletedAttachments.remove(attachment);
 			}
 		}
 		// Remove from database deleted attachments
 		for (Attachment deletedAttachment : deletedAttachments) {
 			StorageManager.delete(mActivity, deletedAttachment.getUri().getPath());
+            Ln.d("Removed attachment " + deletedAttachment.getUri());
 		}
 	}
 
-	
-	@Override
+    private Attachment getFixedAttachmentInstance(List<Attachment> deletedAttachments, Attachment attachment) {
+        for (Attachment deletedAttachment : deletedAttachments) {
+            if (deletedAttachment.getId() == attachment.getId()) return deletedAttachment;
+        }
+        return attachment;
+    }
+
+
+    @Override
 	protected void onPostExecute(Note note) {
 		super.onPostExecute(note);
-		
+
 		// Set reminder if is not passed yet
 		long now = Calendar.getInstance().getTimeInMillis();
 		if (note.getAlarm() != null && Long.parseLong(note.getAlarm()) >= now) {
@@ -85,8 +98,8 @@ public class SaveNoteTask extends AsyncTask<Note, Void, Note> {
 			mOnNoteSaved.onNoteSaved(note);
 		}
 	}
-	
-	
+
+
 
 	private void setAlarm(Note note) {
 		Intent intent = new Intent(mActivity, AlarmReceiver.class);
@@ -96,6 +109,6 @@ public class SaveNoteTask extends AsyncTask<Note, Void, Note> {
 		AlarmManager am = (AlarmManager) mActivity.getSystemService(Activity.ALARM_SERVICE);
 		am.set(AlarmManager.RTC_WAKEUP, Long.parseLong(note.getAlarm()), sender);
 	}
-	
-	
+
+
 }
