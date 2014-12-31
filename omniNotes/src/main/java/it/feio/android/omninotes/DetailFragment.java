31e3f0c97bb69786708aaad366089abe25f7abde
Federico Iosue <federico.iosue@gmail.com>
2014-12-31 11:39:38 +0100

    Updated copyright moving from Apache v2 to GPL v3

--- omniNotes/src/main/java/it/feio/android/omninotes/DetailFragment.java
+++ omniNotes/src/main/java/it/feio/android/omninotes/DetailFragment.java
@@ -1,18 +1,19 @@
-/*******************************************************************************
- * Copyright 2014 Federico Iosue (federico.iosue@gmail.com)
+/*
+ * Copyright (C) 2015 Federico Iosue (federico.iosue@gmail.com)
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use getActivity() file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ******************************************************************************/
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package it.feio.android.omninotes;
 
 import android.annotation.SuppressLint;
@@ -1897,9 +1898,11 @@ public class DetailFragment extends Fragment implements
                 public void onAnimationStart(Animation animation) {
                 }
 
+
                 @Override
                 public void onAnimationRepeat(Animation animation) {
                 }
+
 
                 @Override
                 public void onAnimationEnd(Animation animation) {
@@ -2109,7 +2112,8 @@ public class DetailFragment extends Fragment implements
     public void onReminderPicked(long reminder) {
         noteTmp.setAlarm(reminder);
         if (mFragment.isAdded()) {
-            datetime.setText(getString(R.string.alarm_set_on) + " " + DateHelper.getDateTimeShort(getActivity(), reminder));
+            datetime.setText(getString(R.string.alarm_set_on) + " " + DateHelper.getDateTimeShort(getActivity(),
+                    reminder));
         }
     }
 
