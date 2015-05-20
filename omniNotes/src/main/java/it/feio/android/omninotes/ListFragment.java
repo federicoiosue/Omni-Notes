508e0223e1a6b7e9af13c8ad43ae820a39d4c3c3
Federico Iosue <federico.iosue@gmail.com>
2015-05-20 17:56:07 +0200

    Fix broken checklist tagging

--- omniNotes/src/main/java/it/feio/android/omninotes/ListFragment.java
+++ omniNotes/src/main/java/it/feio/android/omninotes/ListFragment.java
@@ -1458,7 +1458,7 @@ public class ListFragment extends BaseFragment implements OnViewTouchedListener,
         Pair<String, List<Tag>> taggingResult = TagsHelper.addTagToNote(tags, selectedTags, note);
 
         if (note.isChecklist()) {
-            note.setTitle(note.getContent() + System.getProperty("line.separator") + taggingResult.first);
+            note.setTitle(note.getTitle() + System.getProperty("line.separator") + taggingResult.first);
         } else {
             StringBuilder sb = new StringBuilder(note.getContent());
             if (sb.length() > 0) {
