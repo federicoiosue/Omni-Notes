36a29084dfc03664b40abfad836d130ed7e731a8
Federico Iosue <federico.iosue@gmail.com>
2015-05-22 08:34:26 +0200

    Modified DashClock service to avoid some db queries

--- omniNotes/src/main/java/it/feio/android/omninotes/async/ONDashClockExtension.java
+++ omniNotes/src/main/java/it/feio/android/omninotes/async/ONDashClockExtension.java
@@ -31,12 +31,14 @@ import it.feio.android.omninotes.db.DbHelper;
 import it.feio.android.omninotes.models.Note;
 import it.feio.android.omninotes.utils.Constants;
 import it.feio.android.omninotes.utils.TextHelper;
+import it.feio.android.omninotes.utils.date.DateHelper;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 
 public class ONDashClockExtension extends DashClockExtension {
+
+    private enum Counters {ACTIVE, REMINDERS, TODAY};
 
 
     private DashClockUpdateReceiver mDashClockReceiver;
@@ -45,13 +47,9 @@ public class ONDashClockExtension extends DashClockExtension {
     @Override
     protected void onInitialize(boolean isReconnect) {
         super.onInitialize(isReconnect);
-
         LocalBroadcastManager broadcastMgr = LocalBroadcastManager.getInstance(this);
         if (mDashClockReceiver != null) {
-            try {
-                broadcastMgr.unregisterReceiver(mDashClockReceiver);
-            } catch (Exception ignore) {
-            }
+            broadcastMgr.unregisterReceiver(mDashClockReceiver);
         }
         mDashClockReceiver = new DashClockUpdateReceiver();
         broadcastMgr.registerReceiver(mDashClockReceiver, new IntentFilter(Constants.INTENT_UPDATE_DASHCLOCK));
@@ -62,24 +60,19 @@ public class ONDashClockExtension extends DashClockExtension {
     @Override
     protected void onUpdateData(int reason) {
 
-        DbHelper db = DbHelper.getInstance();
-        int notes = db.getNotesActive().size();
-        int reminders = db.getNotesWithReminder(true).size();
-        List<Note> todayReminders = new ArrayList<>();
-        if (reminders > 0) {
-            todayReminders = db.getTodayReminders();
-        }
+        Map<String, List<Note>> notesCounters = getNotesCounters();
+        int reminders = notesCounters.get(Counters.REMINDERS).size();
 
         StringBuilder expandedTitle = new StringBuilder();
-        expandedTitle.append(notes).append(" ").append(getString(R.string.notes).toLowerCase());
+        expandedTitle.append(notesCounters.get(Counters.ACTIVE).size()).append(" ").append(getString(R.string.notes).toLowerCase());
         if (reminders > 0) {
             expandedTitle.append(", ").append(reminders).append(" ").append(getString(R.string.reminders));
         }
 
         StringBuilder expandedBody = new StringBuilder();
-        if (todayReminders.size() > 0) {
-            expandedBody.append(todayReminders.size()).append(" ").append(getString(R.string.today)).append(":");
-            for (Note todayReminder : todayReminders) {
+        if (notesCounters.get(Counters.TODAY).size() > 0) {
+            expandedBody.append(notesCounters.get(Counters.TODAY).size()).append(" ").append(getString(R.string.today)).append(":");
+            for (Note todayReminder : notesCounters.get(Counters.TODAY)) {
                 expandedBody.append(System.getProperty("line.separator")).append(("☆ ")).append(TextHelper
                         .parseTitleAndContent(this, todayReminder)[0]);
             }
@@ -89,10 +82,31 @@ public class ONDashClockExtension extends DashClockExtension {
         publishUpdate(new ExtensionData()
                 .visible(true)
                 .icon(R.drawable.ic_stat_notification_icon)
-                .status(String.valueOf(notes))
+                .status(String.valueOf(notesCounters.get(Counters.ACTIVE)))
                 .expandedTitle(expandedTitle.toString())
                 .expandedBody(expandedBody.toString())
                 .clickIntent(new Intent(this, MainActivity.class)));
+    }
+
+
+    private Map<String, List<Note>> getNotesCounters() {
+        Map noteCounters = new HashMap<>();
+        List<Note> activeNotes = new ArrayList<>();
+        List<Note> reminders = new ArrayList<>();
+        List<Note> today = new ArrayList<>();
+        for (Note note : DbHelper.getInstance().getNotesActive()) {
+            activeNotes.add(note);
+            if (note.getAlarm() != null) {
+                reminders.add(note);
+                if (DateHelper.isSameDay(Long.valueOf(note.getAlarm()), Calendar.getInstance().getTimeInMillis())) {
+                    today.add(note);
+                }
+            }
+        }
+        noteCounters.put(Counters.ACTIVE, activeNotes);
+        noteCounters.put(Counters.REMINDERS, reminders);
+        noteCounters.put(Counters.TODAY, today);
+        return noteCounters;
     }
 
 
--- omniNotes/src/main/java/it/feio/android/omninotes/utils/date/DateHelper.java
+++ omniNotes/src/main/java/it/feio/android/omninotes/utils/date/DateHelper.java
@@ -291,4 +291,14 @@ public class DateHelper {
                 (R.string.starting_from) + " " + DateHelper.getDateTimeShort(OmniNotes.getAppContext(), reminder);
     }
 
+
+    public static boolean isSameDay(long date1, long date2) {
+        Calendar cal1 = Calendar.getInstance();
+        Calendar cal2 = Calendar.getInstance();
+        cal1.setTimeInMillis(date1);
+        cal2.setTimeInMillis(date2);
+        return cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) && cal1.get(Calendar.DAY_OF_YEAR) == cal2.get
+                (Calendar.DAY_OF_YEAR);
+    }
+
 }
