b1ac6cda5ba851e8ca97dce67aa04af45a50b671
Federico Iosue <federico.iosue@gmail.com>
2015-10-22 09:15:03 +0200

    Made use of native JVM behavior to restart app

--- omniNotes/src/main/java/it/feio/android/omninotes/utils/MiscUtils.java
+++ omniNotes/src/main/java/it/feio/android/omninotes/utils/MiscUtils.java
@@ -18,14 +18,14 @@ public class MiscUtils {
 	 * Performs a full app restart
 	 */
 	public static void restartApp(final Context mContext, Class activityClass) {
-		Intent intent = new Intent(mContext, activityClass);
-		intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		int mPendingIntentId = Long.valueOf(Calendar.getInstance().getTimeInMillis()).intValue();
-		PendingIntent mPendingIntent = PendingIntent.getActivity(mContext, mPendingIntentId, intent,
-				PendingIntent.FLAG_CANCEL_CURRENT);
-		AlarmManager mgr = (AlarmManager) mContext.getSystemService(Context.ALARM_SERVICE);
-		mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 100, mPendingIntent);
+//		Intent intent = new Intent(mContext, activityClass);
+//		intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//		int mPendingIntentId = Long.valueOf(Calendar.getInstance().getTimeInMillis()).intValue();
+//		PendingIntent mPendingIntent = PendingIntent.getActivity(mContext, mPendingIntentId, intent,
+//				PendingIntent.FLAG_CANCEL_CURRENT);
+//		AlarmManager mgr = (AlarmManager) mContext.getSystemService(Context.ALARM_SERVICE);
+//		mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 100, mPendingIntent);
 		System.exit(0);
 	}
 }
