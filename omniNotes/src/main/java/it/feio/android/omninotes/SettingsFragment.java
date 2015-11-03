1042d7638a38986e6560b4915b4f6e37e569b349
Federico Iosue <federico.iosue@gmail.com>
2015-11-03 22:45:43 +0100

    Integrated PermissionsHelper in settings for backup (WRITE_EXTERNAL_STORAGE)

--- omniNotes/src/main/java/it/feio/android/omninotes/SettingsFragment.java
+++ omniNotes/src/main/java/it/feio/android/omninotes/SettingsFragment.java
@@ -15,6 +15,7 @@
  ******************************************************************************/
 package it.feio.android.omninotes;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -40,7 +41,9 @@ import android.widget.Toast;
 import com.afollestad.materialdialogs.MaterialDialog;
 import it.feio.android.omninotes.async.DataBackupIntentService;
 import it.feio.android.omninotes.helpers.AnalyticsHelper;
+import it.feio.android.omninotes.helpers.PermissionsHelper;
 import it.feio.android.omninotes.models.ONStyle;
+import it.feio.android.omninotes.models.listeners.OnPermissionRequestedListener;
 import it.feio.android.omninotes.utils.*;
 import org.apache.commons.lang.StringUtils;
 
@@ -133,46 +136,8 @@ public class SettingsFragment extends PreferenceFragment {
 				View v = inflater.inflate(R.layout.dialog_backup_layout, null);
 
 				// Finds actually saved backups names
-				final List<String> backups = Arrays.asList(StorageHelper.getExternalStoragePublicDir().list());
-
-				// Sets default export file name
-				SimpleDateFormat sdf = new SimpleDateFormat(Constants.DATE_FORMAT_EXPORT);
-				String fileName = sdf.format(Calendar.getInstance().getTime());
-				final EditText fileNameEditText = (EditText) v.findViewById(R.id.export_file_name);
-				final TextView backupExistingTextView = (TextView) v.findViewById(R.id.backup_existing);
-				fileNameEditText.setHint(fileName);
-				fileNameEditText.addTextChangedListener(new TextWatcher() {
-					@Override
-					public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {}
-					@Override
-					public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {}
-					@Override
-					public void afterTextChanged(Editable arg0) {
-						if (backups.contains(arg0.toString())) {
-							backupExistingTextView.setText(R.string.backup_existing);
-						} else {
-							backupExistingTextView.setText("");
-						}
-					}
-				});
-
-				new MaterialDialog.Builder(getActivity())
-						.title(R.string.data_export_message)
-						.customView(v, false)
-						.positiveText(R.string.confirm)
-						.callback(new MaterialDialog.ButtonCallback() {
-							@Override
-							public void onPositive(MaterialDialog materialDialog) {
-								AnalyticsHelper.trackEvent(AnalyticsHelper.CATEGORIES.SETTING, "settings_export_data");
-								// An IntentService will be launched to accomplish the export task
-								Intent service = new Intent(getActivity(), DataBackupIntentService.class);
-								service.setAction(DataBackupIntentService.ACTION_DATA_EXPORT);
-								String backupName = StringUtils.isEmpty(fileNameEditText.getText().toString()) ?
-										fileNameEditText.getHint().toString() : fileNameEditText.getText().toString();
-								service.putExtra(DataBackupIntentService.INTENT_BACKUP_NAME, backupName);
-								getActivity().startService(service);
-							}
-						}).build().show();
+				PermissionsHelper.requestPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE,
+						"Stianta", activity.findViewById(R.id.crouton_handle), () -> export(v));
 
 				return false;
 			});
@@ -629,6 +594,50 @@ public class SettingsFragment extends PreferenceFragment {
 //                }
 //            });
 //        }
+	}
+
+
+	private void export(View v) {
+		final List<String> backups = Arrays.asList(StorageHelper.getExternalStoragePublicDir().list());
+
+		// Sets default export file name
+		SimpleDateFormat sdf = new SimpleDateFormat(Constants.DATE_FORMAT_EXPORT);
+		String fileName = sdf.format(Calendar.getInstance().getTime());
+		final EditText fileNameEditText = (EditText) v.findViewById(R.id.export_file_name);
+		final TextView backupExistingTextView = (TextView) v.findViewById(R.id.backup_existing);
+		fileNameEditText.setHint(fileName);
+		fileNameEditText.addTextChangedListener(new TextWatcher() {
+			@Override
+			public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {}
+			@Override
+			public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {}
+			@Override
+			public void afterTextChanged(Editable arg0) {
+				if (backups.contains(arg0.toString())) {
+					backupExistingTextView.setText(R.string.backup_existing);
+				} else {
+					backupExistingTextView.setText("");
+				}
+			}
+		});
+
+		new MaterialDialog.Builder(getActivity())
+				.title(R.string.data_export_message)
+				.customView(v, false)
+				.positiveText(R.string.confirm)
+				.callback(new MaterialDialog.ButtonCallback() {
+					@Override
+					public void onPositive(MaterialDialog materialDialog) {
+						AnalyticsHelper.trackEvent(AnalyticsHelper.CATEGORIES.SETTING, "settings_export_data");
+						// An IntentService will be launched to accomplish the export task
+						Intent service = new Intent(getActivity(), DataBackupIntentService.class);
+						service.setAction(DataBackupIntentService.ACTION_DATA_EXPORT);
+						String backupName = StringUtils.isEmpty(fileNameEditText.getText().toString()) ?
+								fileNameEditText.getHint().toString() : fileNameEditText.getText().toString();
+						service.putExtra(DataBackupIntentService.INTENT_BACKUP_NAME, backupName);
+						getActivity().startService(service);
+					}
+				}).build().show();
 	}
 
 
