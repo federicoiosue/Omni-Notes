481490aeba64c05191724316dc8ac9c2b4e42f11
Federico Iosue <federico.iosue@gmail.com>
2016-06-01 00:06:33 +0200

    Reverted attachments copying to single thread to avoid strange artifacts

--- omniNotes/src/main/java/it/feio/android/omninotes/DetailFragment.java
+++ omniNotes/src/main/java/it/feio/android/omninotes/DetailFragment.java
@@ -489,7 +489,7 @@ public class DetailFragment extends BaseFragment implements OnReminderPickedList
 			// audio recording attached the case must be handled in specific way
 			if (uri != null && !Constants.INTENT_GOOGLE_NOW.equals(i.getAction())) {
 				String name = FileHelper.getNameFromUri(mainActivity, uri);
-				new AttachmentTask(this, uri, name, this).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+				new AttachmentTask(this, uri, name, this).execute();
 			}
 
 			// Multiple attachment data
@@ -497,7 +497,7 @@ public class DetailFragment extends BaseFragment implements OnReminderPickedList
 			if (uris != null) {
 				for (Uri uriSingle : uris) {
 					String name = FileHelper.getNameFromUri(mainActivity, uriSingle);
-					new AttachmentTask(this, uriSingle, name, this).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+					new AttachmentTask(this, uriSingle, name, this).execute();
 				}
 			}
 		}
