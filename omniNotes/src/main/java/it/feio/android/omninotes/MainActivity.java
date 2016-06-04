ba29d51a6c7a5c73906202ae50ef3d6000c48975
Federico Iosue <federico.iosue@gmail.com>
2016-06-04 23:52:16 +0200

    Added missing class import

--- omniNotes/src/main/java/it/feio/android/omninotes/MainActivity.java
+++ omniNotes/src/main/java/it/feio/android/omninotes/MainActivity.java
@@ -24,6 +24,7 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
