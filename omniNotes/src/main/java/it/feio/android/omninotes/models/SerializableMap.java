63b0300d53fa623c9ddcf0494941944eaaa9fdb5
Federico Iosue <federico.iosue@gmail.com>
2016-06-21 21:28:39 +0200

    Removed unused classes

--- omniNotes/src/main/java/it/feio/android/omninotes/models/SerializableMap.java
+++
@@ -1,9 +0,0 @@
-package it.feio.android.omninotes.models;
-
-import java.io.Serializable;
-import java.util.Map;
-
-/**
- * Created by georgekankava on 3/3/16.
- */
-public interface SerializableMap<K, V> extends Map<K, V>, Serializable {}
