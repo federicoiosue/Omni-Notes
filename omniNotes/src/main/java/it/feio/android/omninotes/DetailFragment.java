e538db1b45997ff54a5ec070f64a767c29e21057
Federico Iosue <federico.iosue@gmail.com>
2016-07-07 23:02:43 +0200

    Fixed broken location address setting
    Why this was been commented...?

--- omniNotes/src/main/java/it/feio/android/omninotes/DetailFragment.java
+++ omniNotes/src/main/java/it/feio/android/omninotes/DetailFragment.java
@@ -926,7 +926,7 @@ public class DetailFragment extends BaseFragment implements OnReminderPickedList
 								if (TextUtils.isEmpty(autoCompView.getText().toString())) {
 									noteTmp.setLatitude(location.getLatitude());
 									noteTmp.setLongitude(location.getLongitude());
-//									GeocodeHelper.getAddressFromCoordinates(location, mFragment);
+									GeocodeHelper.getAddressFromCoordinates(location, mFragment);
 								} else {
 									GeocodeHelper.getCoordinatesFromAddress(autoCompView.getText().toString(),
 											mFragment);
