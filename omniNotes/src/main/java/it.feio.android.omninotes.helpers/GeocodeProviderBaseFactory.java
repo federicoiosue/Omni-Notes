6aada3c614f034c7b03d58d427c257e642d5d6c9
Federico Iosue <federico.iosue@dada.eu>
2017-02-12 09:44:43 +0100

    Fixed smart-location-lib clone's providers factory

--- omniNotes/src/main/java/it.feio.android.omninotes.helpers/GeocodeProviderBaseFactory.java
+++ omniNotes/src/main/java/it.feio.android.omninotes.helpers/GeocodeProviderBaseFactory.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import io.nlopez.smartlocation.location.LocationProvider;
 import io.nlopez.smartlocation.location.providers.LocationGooglePlayServicesWithFallbackProvider;
 
-public class GeoCodeProviderFactory {
+public class GeocodeProviderBaseFactory {
 
     public static LocationProvider getProvider(Context context) {
         return new LocationGooglePlayServicesWithFallbackProvider(context);
