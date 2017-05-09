635fe9c4f011e52b9ef117c70c9a4ddb9f099824
Federico Iosue <federico.iosue@dada.eu>
2017-05-09 17:02:57 +0200

    Managed old version check string-based

--- omniNotes/src/androidTest/java/it/feio/android/omninotes/helpers/AppVersionHelperTest.java
+++ omniNotes/src/androidTest/java/it/feio/android/omninotes/helpers/AppVersionHelperTest.java
@@ -54,5 +54,16 @@ public class AppVersionHelperTest extends BaseAndroidTestCase {
         assertTrue(currentAppVersionName.matches(VERSION_NAME_REGEX));
     }
 
+    public void testLegacyVersionManagementRetrieval() throws PackageManager.NameNotFoundException {
+        prefs.edit().putString(Constants.PREF_CURRENT_APP_VERSION, "5.2.6 Beta 1").commit();
+        int currentAppVersion = AppVersionHelper.getCurrentAppVersion(testContext);
+        int saveAppVersion = AppVersionHelper.getAppVersionFromPreferences(testContext);
+        assertEquals(currentAppVersion - 1, saveAppVersion);
+    }
+
+    public void testLegacyVersionManagementUpdateCheck() throws PackageManager.NameNotFoundException {
+        prefs.edit().putString(Constants.PREF_CURRENT_APP_VERSION, "5.2.6 Beta 1").commit();
+        assertTrue(AppVersionHelper.isAppUpdated(testContext));
+    }
 
 }
--- omniNotes/src/main/java/it/feio/android/omninotes/helpers/AppVersionHelper.java
+++ omniNotes/src/main/java/it/feio/android/omninotes/helpers/AppVersionHelper.java
@@ -30,30 +30,34 @@ import it.feio.android.omninotes.utils.Constants;
  */
 public class AppVersionHelper {
 
-	public static boolean isAppUpdated(Context context) throws PackageManager.NameNotFoundException {
-		int currentAppVersion = getCurrentAppVersion(context);
-		int savedAppVersion = getAppVersionFromPreferences(context);
-		return currentAppVersion > savedAppVersion;
-	}
+    public static boolean isAppUpdated(Context context) throws PackageManager.NameNotFoundException {
+        int currentAppVersion = getCurrentAppVersion(context);
+        int savedAppVersion = getAppVersionFromPreferences(context);
+        return currentAppVersion > savedAppVersion;
+    }
 
-	public static int getAppVersionFromPreferences(Context context) {
-		return context.getSharedPreferences(Constants.PREFS_NAME,
-				Context.MODE_MULTI_PROCESS).getInt(Constants.PREF_CURRENT_APP_VERSION, 1);
-	}
+    public static int getAppVersionFromPreferences(Context context) throws PackageManager.NameNotFoundException {
+        try {
+            return context.getSharedPreferences(Constants.PREFS_NAME,
+                    Context.MODE_MULTI_PROCESS).getInt(Constants.PREF_CURRENT_APP_VERSION, 1);
+        } catch (ClassCastException e) {
+            return getCurrentAppVersion(context) - 1;
+        }
+    }
 
-	public static void updateAppVersionInPreferences(Context context) throws PackageManager.NameNotFoundException {
-		context.getSharedPreferences(Constants.PREFS_NAME,
-				Context.MODE_MULTI_PROCESS).edit().putInt(Constants.PREF_CURRENT_APP_VERSION,
-				getCurrentAppVersion(context)).apply();
-	}
+    public static void updateAppVersionInPreferences(Context context) throws PackageManager.NameNotFoundException {
+        context.getSharedPreferences(Constants.PREFS_NAME,
+                Context.MODE_MULTI_PROCESS).edit().putInt(Constants.PREF_CURRENT_APP_VERSION,
+                getCurrentAppVersion(context)).apply();
+    }
 
-	public static int getCurrentAppVersion(Context context) throws PackageManager.NameNotFoundException {
-		return context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionCode;
-	}
+    public static int getCurrentAppVersion(Context context) throws PackageManager.NameNotFoundException {
+        return context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionCode;
+    }
 
-	public static String getCurrentAppVersionName(Context context) throws PackageManager.NameNotFoundException {
-		PackageInfo pInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
-		return pInfo.versionName;
-	}
+    public static String getCurrentAppVersionName(Context context) throws PackageManager.NameNotFoundException {
+        PackageInfo pInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+        return pInfo.versionName;
+    }
 
 }
