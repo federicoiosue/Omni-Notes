2a3b91a6924c1bef9d414193ce4acf2adf3915be
Federico Iosue <federico.iosue@gmail.com>
2017-11-01 03:29:54 +0100

    Fixed tests

--- omniNotes/src/androidTest/java/it/feio/android/omninotes/CategoryLifecycleTest.java
+++ omniNotes/src/androidTest/java/it/feio/android/omninotes/CategoryLifecycleTest.java
@@ -120,8 +120,7 @@ public class CategoryLifecycleTest extends BaseEspressoTest {
 
         ViewInteraction drawerToggle = onView(
                 allOf(withContentDescription("drawer open"),
-                        withParent(withId(R.id.toolbar)),
-                        isDisplayed()));
+                        withParent(withId(R.id.toolbar))));
         drawerToggle.perform(click());
 
         ViewInteraction categoryView = onView(allOf(withId(R.id.title), withText(categoryName)));
