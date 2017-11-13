bff0c32bd55f69668576df01ce1def3ba0d0d162
User <user@mail.local>
2017-11-13 23:53:15 +0300

    Fixed: Note metadata hidden behind Android navigation #365

--- omniNotes/src/main/java/it/feio/android/omninotes/utils/Display.java
+++ omniNotes/src/main/java/it/feio/android/omninotes/utils/Display.java
@@ -146,5 +146,20 @@ public class Display {
 		return context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
 	}
 
+    public static int getSoftButtonsBarHeight(Activity activity) {
+        // getRealMetrics is only available with API 17 and +
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            DisplayMetrics metrics = new DisplayMetrics();
+            activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+            int usableHeight = metrics.heightPixels;
+            activity.getWindowManager().getDefaultDisplay().getRealMetrics(metrics);
+            int realHeight = metrics.heightPixels;
+            if (realHeight > usableHeight)
+                return realHeight - usableHeight;
+            else
+                return 0;
+        }
+        return 0;
+    }
 
 }
