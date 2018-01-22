f9f16898d2e406251e967d71db7c6ad2cbac0cc9
Richard Zhang <zhang.richard2016@gmail.com>
2018-01-21 22:48:15 -0500

    removed issue for on long click reminder deletion method

--- omniNotes/src/main/java/it/feio/android/omninotes/DetailFragment.java
+++ omniNotes/src/main/java/it/feio/android/omninotes/DetailFragment.java
@@ -577,7 +577,7 @@ public class DetailFragment extends BaseFragment implements OnReminderPickedList
 					.callback(new MaterialDialog.ButtonCallback() {
 						@Override
 						public void onPositive(MaterialDialog materialDialog) {
-							noteTmp.setAlarm(null);
+							ReminderHelper.removeReminder(OmniNotes.getAppContext(), noteTmp);
 							reminderIcon.setImageResource(R.drawable.ic_alarm_black_18dp);
 							datetime.setText("");
 						}
