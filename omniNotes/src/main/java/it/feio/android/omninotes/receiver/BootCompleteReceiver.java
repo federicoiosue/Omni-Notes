0ff5c4e49787b83af0c9ac9e9beb6c156e857543
Federico Iosue <federico.iosue@dada.eu>
2019-01-21 01:12:32 +0100

    Fix crash when trying to start service from background on Oreo (fix #652)

--- omniNotes/src/main/java/it/feio/android/omninotes/receiver/BootCompleteReceiver.java
+++ omniNotes/src/main/java/it/feio/android/omninotes/receiver/BootCompleteReceiver.java
@@ -20,6 +20,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+
 import it.feio.android.omninotes.async.AlarmRestoreOnRebootService;
 import it.feio.android.omninotes.utils.Constants;
 
@@ -29,12 +30,9 @@ public class BootCompleteReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context ctx, Intent intent) {
         Log.i(Constants.TAG, "System rebooted: refreshing reminders");
-
-        if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
-            Intent service = new Intent(ctx, AlarmRestoreOnRebootService.class);
-            ctx.startService(service);
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            AlarmRestoreOnRebootService.enqueueWork(ctx, intent);
         }
-
     }
 
 
