e5af319c4e657618edf3c630e5b3530569117fa5
Alan V <vidal_a12@lecnam.net>
2019-11-13 21:16:56 +0100

    fix notification err channel

--- omniNotes/src/main/java/it/feio/android/omninotes/utils/notifications/NotificationChannels.java
+++ omniNotes/src/main/java/it/feio/android/omninotes/utils/notifications/NotificationChannels.java
@@ -20,28 +20,35 @@ package it.feio.android.omninotes.utils.notifications;
 import android.annotation.TargetApi;
 import android.app.NotificationManager;
 import android.os.Build;
-import it.feio.android.omninotes.OmniNotes;
-import it.feio.android.omninotes.R;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import it.feio.android.omninotes.OmniNotes;
+import it.feio.android.omninotes.R;
 
 @TargetApi(Build.VERSION_CODES.O)
 public class NotificationChannels {
 
-  private static List<NotificationChannel> channels = new ArrayList<NotificationChannel>() {{
-    add(new NotificationChannel(NotificationManager.IMPORTANCE_DEFAULT, OmniNotes.getAppContext().getString(R.string
-        .channel_backups_name), OmniNotes.getAppContext().getString(R.string
-        .channel_backups_description)));
-    add(new NotificationChannel(NotificationManager.IMPORTANCE_DEFAULT, OmniNotes.getAppContext().getString(R.string
-        .channel_reminders_name), OmniNotes.getAppContext().getString(R.string
-        .channel_reminders_description)));
-  }};
+    private static List<NotificationChannel> channels = new ArrayList<NotificationChannel>() {{
+        add(new NotificationChannel(
+                NotificationManager.IMPORTANCE_DEFAULT,
+                OmniNotes.getAppContext().getString(R.string.channel_backups_name),
+                OmniNotes.getAppContext().getString(R.string.channel_backups_description),
+                OmniNotes.getAppContext().getString(R.string.channel_backups_id)));
+        add(new NotificationChannel(
+                NotificationManager.IMPORTANCE_DEFAULT,
+                OmniNotes.getAppContext().getString(R.string.channel_reminders_name),
+                OmniNotes.getAppContext().getString(R.string.channel_reminders_description),
+                OmniNotes.getAppContext().getString(R.string.channel_reminders_id)
+        ));
+    }};
 
-  public static List<NotificationChannel> getChannels () {
-    return channels;
-  }
+    public static List<NotificationChannel> getChannels() {
+        return channels;
+    }
 
-  public enum NotificationChannelNames {
-    Backups, Reminders
-  }
+    public enum NotificationChannelNames {
+        Backups, Reminders
+    }
 }
--- omniNotes/src/main/java/it/feio/android/omninotes/utils/notifications/NotificationsHelper.java
+++ omniNotes/src/main/java/it/feio/android/omninotes/utils/notifications/NotificationsHelper.java
@@ -31,160 +31,161 @@ import android.net.Uri;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationCompat.Builder;
+
 import it.feio.android.omninotes.R;
 
 
 public class NotificationsHelper {
 
-  private Context mContext;
-  private Builder mBuilder;
-  private NotificationManager mNotificationManager;
+    private Context mContext;
+    private Builder mBuilder;
+    private NotificationManager mNotificationManager;
 
-  public NotificationsHelper (Context mContext) {
-    this.mContext = mContext.getApplicationContext();
-    if (mNotificationManager == null) {
-      mNotificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
-    }
-  }
-
-  /**
-   * Create the NotificationChannel, but only on API 26+ because the NotificationChannel class is new and not in the
-   * support library
-   */
-  @TargetApi(Build.VERSION_CODES.O)
-  public void initNotificationChannels () {
-    if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
-      for (it.feio.android.omninotes.utils.notifications.NotificationChannel notificationChannel : NotificationChannels.getChannels()) {
-        NotificationChannel channel = new NotificationChannel(notificationChannel.name, notificationChannel
-            .name, notificationChannel.importance);
-        channel.setDescription(notificationChannel.description);
-        mNotificationManager.createNotificationChannel(channel);
-      }
-    }
-  }
-
-  /**
-   * Creation of notification on operations completed
-   */
-  public NotificationsHelper createNotification (NotificationChannels.NotificationChannelNames channelId, int
-      smallIcon, String title, PendingIntent notifyIntent) {
-    mBuilder = new NotificationCompat.Builder(mContext, channelId.name()).setSmallIcon(smallIcon).setContentTitle(title)
-                                                                         .setAutoCancel(true).setColor(
-            mContext.getResources().getColor(R.color.colorAccent));
-    mBuilder.setContentIntent(notifyIntent);
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-      setLargeIcon(R.drawable.logo_notification_lollipop);
-    } else {
-      setLargeIcon(R.mipmap.ic_launcher);
+    public NotificationsHelper(Context mContext) {
+        this.mContext = mContext.getApplicationContext();
+        if (mNotificationManager == null) {
+            mNotificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+        }
     }
 
-    return this;
-  }
-
-  public Builder getBuilder () {
-    return mBuilder;
-  }
-
-  public NotificationsHelper setLargeIcon (Bitmap largeIconBitmap) {
-    mBuilder.setLargeIcon(largeIconBitmap);
-    return this;
-  }
-
-  public NotificationsHelper setLargeIcon (int largeIconResource) {
-    Bitmap largeIconBitmap = BitmapFactory.decodeResource(mContext.getResources(), largeIconResource);
-    return setLargeIcon(largeIconBitmap);
-  }
-
-  public NotificationsHelper setRingtone (String ringtone) {
-    if (ringtone != null) {
-      mBuilder.setSound(Uri.parse(ringtone));
+    /**
+     * Create the NotificationChannel, but only on API 26+ because the NotificationChannel class is new and not in the
+     * support library
+     */
+    @TargetApi(Build.VERSION_CODES.O)
+    public void initNotificationChannels() {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+            for (it.feio.android.omninotes.utils.notifications.NotificationChannel notificationChannel : NotificationChannels.getChannels()) {
+                NotificationChannel channel = new NotificationChannel(notificationChannel.id, notificationChannel
+                        .name, notificationChannel.importance);
+                channel.setDescription(notificationChannel.description);
+                mNotificationManager.createNotificationChannel(channel);
+            }
+        }
     }
-    return this;
-  }
 
-  public NotificationsHelper setVibration () {
-    return setVibration(null);
-  }
+    /**
+     * Creation of notification on operations completed
+     */
+    public NotificationsHelper createNotification(NotificationChannels.NotificationChannelNames channelId, int
+            smallIcon, String title, PendingIntent notifyIntent) {
+        mBuilder = new NotificationCompat.Builder(mContext, channelId.name()).setSmallIcon(smallIcon).setContentTitle(title)
+                .setAutoCancel(true).setColor(
+                        mContext.getResources().getColor(R.color.colorAccent));
+        mBuilder.setContentIntent(notifyIntent);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            setLargeIcon(R.drawable.logo_notification_lollipop);
+        } else {
+            setLargeIcon(R.mipmap.ic_launcher);
+        }
 
-  public NotificationsHelper setVibration (long[] pattern) {
-    if (pattern == null || pattern.length == 0) {
-      pattern = new long[]{500, 500};
+        return this;
     }
-    mBuilder.setVibrate(pattern);
-    return this;
-  }
 
-  public NotificationsHelper setLedActive () {
-    mBuilder.setLights(Color.BLUE, 1000, 1000);
-    return this;
-  }
-
-  public NotificationsHelper setIcon (int icon) {
-    mBuilder.setSmallIcon(icon);
-    return this;
-  }
-
-  public NotificationsHelper setMessage (String message) {
-    mBuilder.setContentText(message);
-    return this;
-  }
-
-  public NotificationsHelper setIndeterminate () {
-    mBuilder.setProgress(0, 0, true);
-    return this;
-  }
-
-  public NotificationsHelper setOngoing () {
-    mBuilder.setOngoing(true);
-    return this;
-  }
-
-  public NotificationsHelper show () {
-    show(0);
-    return this;
-  }
-
-  public NotificationsHelper show (long id) {
-    Notification mNotification = mBuilder.build();
-    if (mNotification.contentIntent == null) {
-      // Creates a dummy PendingIntent
-      mBuilder.setContentIntent(PendingIntent.getActivity(mContext, 0, new Intent(),
-          PendingIntent.FLAG_UPDATE_CURRENT));
+    public Builder getBuilder() {
+        return mBuilder;
     }
-    // Builds an anonymous Notification object from the builder, and passes it to the NotificationManager
-    mNotificationManager.notify(String.valueOf(id), 0, mBuilder.build());
-    return this;
-  }
 
-  public NotificationsHelper start (NotificationChannels.NotificationChannelNames channelId, int
-      smallIcon, String title) {
-    createNotification(channelId, smallIcon, title, null).setIndeterminate().setOngoing();
-    mNotificationManager.notify(0, mBuilder.setOnlyAlertOnce(true).build());
-    return this;
-  }
+    public NotificationsHelper setLargeIcon(Bitmap largeIconBitmap) {
+        mBuilder.setLargeIcon(largeIconBitmap);
+        return this;
+    }
 
-  public void updateMessage (String message) {
-    updateMessage(0, message);
-  }
+    public NotificationsHelper setLargeIcon(int largeIconResource) {
+        Bitmap largeIconBitmap = BitmapFactory.decodeResource(mContext.getResources(), largeIconResource);
+        return setLargeIcon(largeIconBitmap);
+    }
 
-  public void updateMessage (int id, String message) {
-    mNotificationManager.notify(id, mBuilder.setContentText(message).build());
-  }
+    public NotificationsHelper setRingtone(String ringtone) {
+        if (ringtone != null) {
+            mBuilder.setSound(Uri.parse(ringtone));
+        }
+        return this;
+    }
 
-  public void finish (Intent intent, String message) {
-    finish(0, intent, message);
-  }
+    public NotificationsHelper setVibration() {
+        return setVibration(null);
+    }
 
-  public void finish (int id, Intent intent, String message) {
-    mBuilder.setContentTitle(message).setProgress(0, 0, false).setOngoing(false);
-    mNotificationManager.notify(id, mBuilder.build());
-  }
+    public NotificationsHelper setVibration(long[] pattern) {
+        if (pattern == null || pattern.length == 0) {
+            pattern = new long[]{500, 500};
+        }
+        mBuilder.setVibrate(pattern);
+        return this;
+    }
 
-  public void cancel () {
-    mNotificationManager.cancel(0);
-  }
+    public NotificationsHelper setLedActive() {
+        mBuilder.setLights(Color.BLUE, 1000, 1000);
+        return this;
+    }
 
-  public void cancel (int id) {
-    mNotificationManager.cancel(id);
-  }
+    public NotificationsHelper setIcon(int icon) {
+        mBuilder.setSmallIcon(icon);
+        return this;
+    }
+
+    public NotificationsHelper setMessage(String message) {
+        mBuilder.setContentText(message);
+        return this;
+    }
+
+    public NotificationsHelper setIndeterminate() {
+        mBuilder.setProgress(0, 0, true);
+        return this;
+    }
+
+    public NotificationsHelper setOngoing() {
+        mBuilder.setOngoing(true);
+        return this;
+    }
+
+    public NotificationsHelper show() {
+        show(0);
+        return this;
+    }
+
+    public NotificationsHelper show(long id) {
+        Notification mNotification = mBuilder.build();
+        if (mNotification.contentIntent == null) {
+            // Creates a dummy PendingIntent
+            mBuilder.setContentIntent(PendingIntent.getActivity(mContext, 0, new Intent(),
+                    PendingIntent.FLAG_UPDATE_CURRENT));
+        }
+        // Builds an anonymous Notification object from the builder, and passes it to the NotificationManager
+        mNotificationManager.notify(String.valueOf(id), 0, mBuilder.build());
+        return this;
+    }
+
+    public NotificationsHelper start(NotificationChannels.NotificationChannelNames channelId, int
+            smallIcon, String title) {
+        createNotification(channelId, smallIcon, title, null).setIndeterminate().setOngoing();
+        mNotificationManager.notify(0, mBuilder.setOnlyAlertOnce(true).build());
+        return this;
+    }
+
+    public void updateMessage(String message) {
+        updateMessage(0, message);
+    }
+
+    public void updateMessage(int id, String message) {
+        mNotificationManager.notify(id, mBuilder.setContentText(message).build());
+    }
+
+    public void finish(Intent intent, String message) {
+        finish(0, intent, message);
+    }
+
+    public void finish(int id, Intent intent, String message) {
+        mBuilder.setContentTitle(message).setProgress(0, 0, false).setOngoing(false);
+        mNotificationManager.notify(id, mBuilder.build());
+    }
+
+    public void cancel() {
+        mNotificationManager.cancel(0);
+    }
+
+    public void cancel(int id) {
+        mNotificationManager.cancel(id);
+    }
 }
