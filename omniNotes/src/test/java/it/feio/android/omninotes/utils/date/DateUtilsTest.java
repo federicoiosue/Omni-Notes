1bdb9579317b89ff6045127705cf035fcf8911fd
Federico Iosue <federico.iosue@gmail.com>
2020-06-05 11:03:01 +0200

    Fixed unauthorized class access by Powermock on Java >= 10

--- omniNotes/src/test/java/it/feio/android/omninotes/utils/date/DateUtilsTest.java
+++ omniNotes/src/test/java/it/feio/android/omninotes/utils/date/DateUtilsTest.java
@@ -30,12 +30,14 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({DateUtils.class})
+@PowerMockIgnore("jdk.internal.reflect.*")
 public class DateUtilsTest {
 
   @Test
