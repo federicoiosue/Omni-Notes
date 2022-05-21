c4c4d0792f2306be92612c5390578ee3a7edecee
kiminsu_1306 <kdcson0613@pusan.ac.kr>
2022-05-21 21:38:46 +0900

    [Refactor][ListFragment] variable extract in onCreateView()

--- omniNotes/src/main/java/it/feio/android/omninotes/ListFragment.java
+++ omniNotes/src/main/java/it/feio/android/omninotes/ListFragment.java
@@ -197,7 +197,8 @@ public class ListFragment extends BaseFragment implements OnViewTouchedListener,
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
     if (savedInstanceState != null) {
-      if (savedInstanceState.containsKey(LIST_VIEW_POSITION)) {
+      boolean isInstanceContainKey = savedInstanceState.containsKey(LIST_VIEW_POSITION);
+      if (isInstanceContainKey) {
         listViewPosition = savedInstanceState.getInt(LIST_VIEW_POSITION);
         listViewPositionOffset = savedInstanceState.getInt(LIST_VIEW_POSITION_OFFSET);
         searchQuery = savedInstanceState.getString("searchQuery");
