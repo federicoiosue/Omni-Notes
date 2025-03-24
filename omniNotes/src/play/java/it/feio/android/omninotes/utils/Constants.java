f10436128c53211871f72bd45a8345fb51107b06
Vinicius-TML <viniciustmlopes@hotmail.com>
2025-03-23 23:08:42 -0300

    finaliza refatoração de interfaces em classes

--- omniNotes/src/play/java/it/feio/android/omninotes/utils/Constants.java
+++ omniNotes/src/play/java/it/feio/android/omninotes/utils/Constants.java
@@ -16,14 +16,17 @@
  */
 package it.feio.android.omninotes.utils;
 
-public interface Constants extends ConstantsBase {
+public final class Constants extends ConstantsBase {
 
-  String TAG = "Omni Notes";
-  String EXTERNAL_STORAGE_FOLDER = "Omni Notes";
-  String PACKAGE = "it.feio.android.omninotes";
+  public static final String TAG = "Omni Notes";
+  public static final String EXTERNAL_STORAGE_FOLDER = "Omni Notes";
+  public static final String PACKAGE = "it.feio.android.omninotes";
 
-  String CHANNEL_BACKUPS_ID = PACKAGE + ".backups";
-  String CHANNEL_REMINDERS_ID = PACKAGE + ".reminders";
-  String CHANNEL_PINNED_ID = PACKAGE + ".pinned";
+  public static final String CHANNEL_BACKUPS_ID = PACKAGE + ".backups";
+  public static final String CHANNEL_REMINDERS_ID = PACKAGE + ".reminders";
+  public static final String CHANNEL_PINNED_ID = PACKAGE + ".pinned";
 
+  private Constants() {
+    throw new AssertionError("Cannot be instantiated");
+  }
 }
